1e965e08c5356c807416d3bab67b03c27f643c77
Moein Alizadeh <moein.alizade@gmail.com>
2018-07-01 17:23:10 +0430

    after test commit

--- smsir-wordpress.php
+++ smsir-wordpress.php
@@ -5,6 +5,33 @@ Plugin URI:   https://www.sms.ir/smsir-wordpress
 Description:  کاملترین پلاگین ارسال پیامک با امکانات بسیار زیاد
 Version:      1.0.0
 Author:       Moein Alizadeh
-Author URI:   https://TrueMoein.ir
+Author URI:   https://Keyad.ir/
 License:      GPL2
 */
+
+include_once __DIR__ . '/Class/ChangePostComments.php';
+
+add_action('admin_menu', 'tmoein_register_menus');
+
+function show_menu() {
+	echo 'inam az in';
+}
+
+function tmoein_register_menus() {
+	add_menu_page(
+		'پنل مدیریت پیامک sms.ir',
+		'پیامک sms.ir',
+		'manage_options',
+		'smsir-admin',
+		'show_menu',
+		plugins_url('/smsir-wordpress/assets/img/brand/sms_icon.png'),
+		20
+	);
+}
+
+
+
+
+
+
+
